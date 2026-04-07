40b484f69ae0ec214f06f9efd7eec6500becec56
AnshikaKumrawat <kumrawatanshika@gmail.com>
2026-04-07 15:10:52 +0530

    Update SignUpRequestDto.java

--- src/main/java/com/finance_dashboard/backend/dto/requests/SignUpRequestDto.java
+++ src/main/java/com/finance_dashboard/backend/dto/requests/SignUpRequestDto.java
@@ -1,4 +1,4 @@
-package com.fullStack.expenseTracker.dto.requests;
+package com.finance_dashboard.backend.dto.requests;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
